2a01e2371094386c027443e3b96bb6dc7c61186a
Akshay Nair <phenax5@gmail.com>
2021-11-06 16:23:10 +0530

    chore: adds pretty printing for debugging

--- src/Efyu/Utils.hs
+++ src/Efyu/Utils.hs
@@ -1,10 +1,15 @@
 module Efyu.Utils where
 
 import qualified Data.Map as Map
+import qualified Data.Text.Lazy as T
 import Debug.Trace (trace)
+import qualified Text.Pretty.Simple as Pretty
+
+pShow :: (Show a) => a -> String
+pShow = T.unpack . Pretty.pShow
 
 debugM :: (Monad m, Show a) => a -> m ()
-debugM = flip trace (pure ()) . ("::::LOG:::: " ++) . show
+debugM = flip trace (pure ()) . ("::::LOG:::: " ++) . pShow
 
 peekM :: (Monad m, Show a, Show b) => b -> m a -> m a
 peekM prefix m = do
