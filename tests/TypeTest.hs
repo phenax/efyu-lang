7620ef501edc73cff0d60ae095989e717c62cf16
Akshay Nair <phenax5@gmail.com>
2021-11-08 14:45:57 +0530

    test: adds more cases for lambda type inference

--- tests/TypeTest.hs
+++ tests/TypeTest.hs
@@ -8,51 +8,76 @@ import Test.Hspec
 import TestHelpers
 
 tests =
-  describe "Type inference" $ do
-    let infer = runTI . inferType Map.empty
+  describe "Type checker" $ do
+    describe "Inference" $ do
+      let infer = runTI . inferType Map.empty
 
-    it "should infer literal types" $ do
-      infer (Literal $ LiteralInt 20) `shouldReturn` Right TInt
-      infer (Literal $ LiteralString "") `shouldReturn` Right TString
-      infer (Literal $ LiteralFloat 20.0) `shouldReturn` Right TFloat
-      infer (Literal $ LiteralBool True) `shouldReturn` Right TBool
+      describe "literals" $ do
+        it "should infer literal types" $ do
+          infer (Literal $ LiteralInt 20) `shouldReturn` Right TInt
+          infer (Literal $ LiteralString "") `shouldReturn` Right TString
+          infer (Literal $ LiteralFloat 20.0) `shouldReturn` Right TFloat
+          infer (Literal $ LiteralBool True) `shouldReturn` Right TBool
 
-    it "should infer lambda types" $ do
-      infer ("x" *->> Literal (LiteralFloat 0.0))
-        `shouldReturn` Right (TLambda (TVar "a0") TFloat)
+      describe "functions" $ do
+        it "should infer lambda types" $ do
+          infer ("x" *->> Literal (LiteralFloat 0.0))
+            `shouldReturn` Right (TLambda (TVar "a0") TFloat)
+          infer ("x" *->> "y" *->> Literal (LiteralFloat 0.0))
+            `shouldReturn` Right (TVar "a0" `tlam` TVar "a1" `tlam` TFloat)
+          infer ("x" *->> "y" *->> Var "y")
+            `shouldReturn` Right (TVar "a0" `tlam` TVar "a1" `tlam` TVar "a1")
+          infer ("x" *->> "y" *->> Var "x")
+            `shouldReturn` Right (TVar "a0" `tlam` TVar "a1" `tlam` TVar "a0")
 
-    it "should infer function application" $ do
-      infer (("x" *->> Literal (LiteralInt 3)) `call` Literal (LiteralFloat 0.0))
-        `shouldReturn` Right TInt
-      infer (("x" *->> Var "x") `call` Literal (LiteralFloat 0.0))
-        `shouldReturn` Right TFloat
-      infer (("x" *->> "y" *->> Var "y") `call` Literal (LiteralFloat 0.0))
-        `shouldReturn` Right (TLambda (TVar "a2") (TVar "a2"))
-      infer
-        ( Let
-            [ ("x", Literal . LiteralInt $ 200),
-              ("id", "x" *->> Var "x")
-            ]
-            (Var "id" `call` Var "x")
-        )
-        `shouldReturn` Right TInt
+        it "should infer function application" $ do
+          infer (("x" *->> Literal (LiteralInt 3)) `call` Literal (LiteralFloat 0.0))
+            `shouldReturn` Right TInt
+          infer (("x" *->> Var "x") `call` Literal (LiteralFloat 0.0))
+            `shouldReturn` Right TFloat
+          infer (("x" *->> "y" *->> Var "y") `call` Literal (LiteralFloat 0.0))
+            `shouldReturn` Right (TLambda (TVar "a2") (TVar "a2"))
+          infer
+            ( Let
+                [ ("x", Literal . LiteralInt $ 200),
+                  ("id", "x" *->> Var "x")
+                ]
+                (Var "id" `call` Var "x")
+            )
+            `shouldReturn` Right TInt
+          infer ("fn" *->> "x" *->> Var "fn" `call` Var "x")
+            `shouldReturn` Right ((TVar "a1" `tlam` TVar "a2") `tlam` TVar "a1" `tlam` TVar "a2")
+          infer
+            ("fn" *->> "pair" *->> Var "pair" `call` (Var "fn" `call` str "val") `call` (Var "fn" `call` int 5))
+            `shouldReturn` Left "unable to unify types: TString and TInt"
 
-    it "should error out for invalid variables" $ do
-      infer (Var "foobar")
-        `shouldReturn` Left "Unbound variable foobar"
-      infer ("x" *->> Var "x1")
-        `shouldReturn` Left "Unbound variable x1"
+      it "should error out for invalid variables" $ do
+        infer (Var "foobar")
+          `shouldReturn` Left "Unbound variable foobar"
+        infer ("x" *->> Var "x1")
+          `shouldReturn` Left "Unbound variable x1"
 
-    it "should infer types from let bindings" $ do
-      infer (Let [("x", Literal . LiteralInt $ 200)] (Var "x"))
-        `shouldReturn` Right TInt
-      infer
-        ( Let
-            [ ("x", Literal . LiteralInt $ 200),
-              ("id", "x" *->> Var "x")
-            ]
-            (Var "id" `call` Var "x")
-        )
-        `shouldReturn` Right TInt
+      it "should infer types from let bindings" $ do
+        infer (Let [("x", Literal . LiteralInt $ 200)] (Var "x"))
+          `shouldReturn` Right TInt
+        infer
+          ( Let
+              [ ("x", Literal . LiteralInt $ 200),
+                ("id", "x" *->> Var "x")
+              ]
+              (Var "id" `call` Var "x")
+          )
+          `shouldReturn` Right TInt
+
+      it "should create substitutions from type annotations" $ do
+        -- infer
+        --   ( Let
+        --       [ ("_", TypeAnnotation "name" TString),
+        --         ("name", int 5)
+        --       ]
+        --       (Var "name")
+        --   )
+        --   `shouldReturn` Right TString
+        infer (TypeAnnotation "name" TInt) `shouldReturn` Right TInt
 
 ---
